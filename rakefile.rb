e3ef2f73419a28884e01882b241d9b90ea0ed3fa
BenHall <ben@benhall.me.uk>
2010-07-26 22:16:02 +0100

    Rake spec file

---
+++ rakefile.rb
@@ -0,0 +1,11 @@
+namespace :specs do
+  require 'spec/rake/spectask'
+
+  @spec_opts = '--colour --format specdoc'
+
+  desc "Run specs for Dolphin"
+  Spec::Rake::SpecTask.new :all do |t|
+    t.spec_files = FileList['spec/**/*_spec.rb']
+  end
+  
+end
