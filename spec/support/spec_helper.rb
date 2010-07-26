a1e8f0354a650cd25692a127dc818c34abca35af
BenHall <ben@benhall.me.uk>
2010-07-26 21:57:51 +0100

    Added spec helper

---
+++ spec/support/spec_helper.rb
@@ -0,0 +1,8 @@
+@root_dir = File.expand_path(File.join(File.dirname(__FILE__), "../../"))
+
+$: << File.join(@root_dir, "lib")
+$: << File.join(@root_dir, "spec")
+$: << File.join(@root_dir, "spec/support")
+
+require 'rubygems'
+require 'spec'
