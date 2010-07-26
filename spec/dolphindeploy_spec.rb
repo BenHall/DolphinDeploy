1e75663d550c803d6d7bb1b6c833a52973e7ee14
BenHall <ben@benhall.me.uk>
2010-07-26 22:16:14 +0100

    First two firsts

---
+++ spec/dolphindeploy_spec.rb
@@ -0,0 +1,6 @@
+require File.join(File.dirname(__FILE__), 'support', 'spec_helper')
+
+describe "Loading Dolphin Deploy" do
+  it "should load successfully when running as IronRuby"
+  it "should raise exception when RUBY_ENGINE is not IronRuby"
+end
