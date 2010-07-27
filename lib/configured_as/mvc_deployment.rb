8bbca5525698cb42e3303859f80102de59e8218b
BenHall <ben@benhall.me.uk>
2010-07-28 00:03:36 +0100

    Added TODO about refactoring the MvcDeployment to be more dynamic

--- lib/configured_as/mvc_deployment.rb
+++ lib/configured_as/mvc_deployment.rb
@@ -4,6 +4,8 @@ class MvcDeployment
   attr_accessor :to
   
   def populate(config, environment_being_deployed)
+    # Should be dynamic. Iterate over all the keys and call the method - for example to([]) - passing in the value set. 
+    # The method then goes self.to.server etc...
     env = config.environment[environment_being_deployed]
     self.environment = environment_being_deployed
     self.host = env.host
