22cc9a25d38564be637d98a661b06e1f99d3f4a1
BenHall <ben@benhall.me.uk>
2010-08-03 23:37:56 +0100

    Temp fix until IIS7 implementation is done

--- lib/IIS/IIS.rb
+++ lib/IIS/IIS.rb
@@ -3,12 +3,13 @@ require 'IISVersion'
 class IIS
   ### Will be executed on the local box, might be able to refactor 'server' out.
   def deploy(server, location, deployment)
-    if(IISVersion.current_version =~ /6/)
+    if(IISVersion.current_version =~ /^6/)
       require 'IIS6'
       iis = IIS6.new
     else
-      require 'IIS7'
-      iis = IIS7.new
+      # Temp
+      require 'IIS6'
+      iis = IIS6.new
     end
     
     iis.deploy(server, location, deployment)
