772b0145e7089103cfddda95a9df17be89ad7890
BenHall <ben@benhall.me.uk>
2010-08-04 00:42:10 +0100

    If site already exists, just update path

--- lib/IIS/IIS6.rb
+++ lib/IIS/IIS6.rb
@@ -1,22 +1,39 @@
 $: << 'external'
 load_assembly 'DolphinDeploy.IIS.IIS6'
+load_assembly 'System.DirectoryServices'
 include DolphinDeploy::IIS::IIS6
+include System::DirectoryServices
     
 class IIS6
   def initialize()    
   end
   
   def deploy(server, location, deployment)
-    create_app_pool(server, deployment.site_name) #Server is only so we can pull out correct path to deploy too
-    create_website(server, location, deployment)
+    unless apppool_exists?(server, deployment)
+      create_app_pool(server, deployment.site_name) #Server is only so we can pull out correct path to deploy too
+    end
+    
+    if(website_exists?(server, deployment))
+      update_website_path(server, location, deployment)
+    else
+      create_website(server, location, deployment)
+    end
   end
   
-  def exists?(server, deployment)
+  def website_exists?(server, deployment)
     website = WebsiteController.new
     website.server = server
     website.name = deployment.site_name
     
     return website.exists
+  end
+  
+  def apppool_exists?(server, deployment)
+    app_pool = AppPoolController.new
+    app_pool.server = server
+    app_pool.name = get_app_pool_name(deployment.site_name)
+    
+    return app_pool.exists
   end
   
   private
@@ -26,6 +43,18 @@ class IIS6
     app_pool.server = server  # Always executed on the local box
     app_pool.name = get_app_pool_name(name)
     app_pool.create
+  end
+  
+  def update_website_path(server, location, deployment)  
+    website = WebsiteController.new
+    website.name = deployment.site_name
+    website.server = server
+    site = website.get_website()
+    
+    path = "IIS://localhost/w3svc/#{site.name}/ROOT"
+    root_site = DirectoryEntry.new(path.to_clr_string)
+    root_site.Properties['Path'][0] = location.to_clr_string
+    root_site.commit_changes()
   end
   
   def create_website(server, location, deployment)
