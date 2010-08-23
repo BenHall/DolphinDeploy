d342ba34762688cd9a447472234c415695d39490
BenHall <ben@benhall.me.uk>
2010-08-23 23:15:43 +0100

    Improved File Manager specs

--- spec/file_manager_spec.rb
+++ spec/file_manager_spec.rb
@@ -2,19 +2,21 @@ require File.join(File.dirname(__FILE__), 'support', 'spec_helper')
 require 'file_manager'
 
 describe FileManager, "swapping configuration" do
+  let(:location) {"test"}
+  
   before do
     @mvc = FileManager.new
   end
   
-  it "should make a web.original.config before replacing file" do   
-    FileUtils.stubs(:mv).with('test/web.systest.config', 'test/web.config')
-    FileUtils.expects(:cp).with('test/web.config', 'test/web.original.config').once
-    @mvc.swap_configs('test', :systest)    
+  it "should copy the web.config to web.original.config before replacing" do       
+    FileUtils.stubs(:mv).with("#{location}/web.systest.config", "#{location}/web.config")
+    FileUtils.expects(:cp).with("#{location}/web.config", "#{location}/web.original.config").once
+    @mvc.swap_configs(location, :systest)    
   end
   
-  it "should swap config for environment to web.config" do  
-    FileUtils.stubs(:cp).with('test/web.config', 'test/web.original.config')
-    FileUtils.expects(:mv).with('test/web.systest.config', 'test/web.config').once
-    @mvc.swap_configs('test', :systest)
+  it "should rename web.config for environment to web.config" do  
+    FileUtils.stubs(:cp).with("#{location}/web.config", "#{location}/web.original.config")
+    FileUtils.expects(:mv).with("#{location}/web.systest.config", "#{location}/web.config").once
+    @mvc.swap_configs(location, :systest)
   end
 end
