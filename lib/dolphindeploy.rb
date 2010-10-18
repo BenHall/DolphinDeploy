52a4d65d0104a38a9f9ba05c232c29509361b87f
BenHall <ben@benhall.me.uk>
2010-10-18 08:03:20 +0100

    Fixed load path issues with gem

--- lib/dolphindeploy.rb
+++ lib/dolphindeploy.rb
@@ -1,9 +1,11 @@
 raise Exception, "Dolphin Deploy is only supported on IronRuby" unless Object.const_defined?(:RUBY_ENGINE) && RUBY_ENGINE == 'ironruby'
 
-$: << 'lib\configured_as'
-$: << 'lib\IIS'
+$: << File.join(File.dirname(__FILE__), 'configured_as')
+$: << File.join(File.dirname(__FILE__), 'IIS')
+$: << File.join(File.dirname(__FILE__), '../external')
 require 'rubygems'  #FIX: Apprantly this is bad practice
 require 'digest'
+require 'mvc_deployment'
 require 'deploymentconfig'
 require 'deploycommandcreator'
 require 'IIS'
